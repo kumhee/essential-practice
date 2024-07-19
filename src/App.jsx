164377ede125227e9b46fde5329f980de5d6261e
kumhee <kumheee95@gmail.com>
2024-07-19 17:58:02 +0900

    Feat: add component & fix code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Result from './components/Result.jsx';
+import {useState} from 'react';
 
 function App() {
+  // Note: user가 입력한 값을 얻고 저장할 상태관리 훅
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  // Note: input 필드에서 값을 변경할 때마다 발생되어야 할 함수 (상태를 업데이트하기 위함)
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue
+      }
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Result input={userInput} />
     </>
   )
 }
---
+++ src/components/Result.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Result({ input }) {
+  console.log(input);
+  return (
+    <p>Results: </p>
+  );
+}
