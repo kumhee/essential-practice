b2694cabb36c1a651b7eff520e2c7f51a55a36cf
kumhee <kumheee95@gmail.com>
2024-07-22 11:12:26 +0900

    Feat: add component & fix code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {useState} from 'react';
 
 function App() {
   // Note: user가 입력한 값을 얻고 저장할 상태관리 훅
+  //  초기 투자금, 연간 투자금, 기대 수익률, 투자기간
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
@@ -12,13 +13,15 @@ function App() {
     duration: 10,
   })
 
+  const inputIsValid = userInput.duration >= 1;
+
   // Note: input 필드에서 값을 변경할 때마다 발생되어야 할 함수 (상태를 업데이트하기 위함)
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
-      return {
+      return{
         ...prevUserInput,
-        [inputIdentifier]: newValue
-      }
+        [inputIdentifier]: + newValue
+      };
     });
   }
 
@@ -26,7 +29,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Result input={userInput} />
+      {!inputIsValid && <p className='center'>Please enter duration greater than zero</p>}
+      {inputIsValid && <Result input={userInput} />}
     </>
   )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Result({ input }) {
-  console.log(input);
+  const resultData = calculateInvestmentResults(input);
+
+  // NOTE: 초기 투자 금액을 계산
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
+
   return (
-    <p>Results: </p>
+    <table id='result'>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {resultData.map((yearData) => {
+          // NOTE: 총 이자
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          // Note: 총 투자자본
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   );
 }
